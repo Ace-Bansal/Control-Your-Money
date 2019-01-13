0aa1acfff4ab137e2ad40873e503320263d90c3c
Ace-Bansal <bansalekansh@gmail.com>
2019-01-13 11:57:09 +0530

    Enable cors.

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose")
+const cors = require("cors")
 
 const Expense = require("./models/expense")
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 app.get("/", function(req, res){
     const obj = {
         name: "Ekansh",
         age: 21
     }
-    res.send(obj)
+    console.log("request receied")
+    res.json(obj)
 })
 
 mongoose.connect("mongodb://localhost/controlYourMoney")
